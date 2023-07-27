9ae428a207780412111b4cee37f05348e7925138
cznaltaf <140110936+cznaltaf@users.noreply.github.com>
2023-07-28 02:47:35 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: grey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+button {
+  margin-top: 20px;
+}
+</style>
+</head>
+<body>
+  <h1> What is <a href="https://topbootsdepot.com/">TopBootsDepot</a> ?</h1>
+  <p> Are you looking for the top footwear in the city for your daily activities? In stores and online, there are a million different shoe brands. However, only a few brands offer high-quality services that satisfy your requirements. ADIDAS is among the top and most well-known companies that provide superior quality and foot protection. </p>
+  <h2>More Info</h2>
+  <p>German sportswear company Adidas Footwear makes athletic clothing. It ranks as the second-largest sportswear company globally and one of the largest in Europe. In 1948, Adolf “Adi” Dassler started it. </p>
+  <h2>Trusted Content</h2>
+  <p>Our training content is trusted and up-to-date, as it is developed and reviewed by industry experts who are passionate about staying current with the latest trends and best practices.</p>
+  <h1>Want to visit the site?</h1>
+  <button style="background-color: #0000FF; color: white; font-size: 20px; padding: 10px 20px;" onclick="window.open('https://topbootsdepot.com/')">Topbootsdepot</button>
+
+</body>
+</html>
